f1007e201ab8bbb1d99f8fa8df1ec45b95adb9d2
nishithay88 <rohith.j721@gmail.com>
2017-11-21 00:17:30 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chat from "./Chat/Chat";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <Chat />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
